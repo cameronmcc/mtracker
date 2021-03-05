9c8325fb6863282dc921cbd73f82b9676efb59bb
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-03-04 16:03:34 -0800

    Models finished except for chat room validation

--- mtracker/main_app/models.py
+++ mtracker/main_app/models.py
@@ -24,13 +24,30 @@ class UserManager(models.Manager):
             errors['password'] = "passwords do not match"
         return errors
 
-class UserMessageManager(self, post_data):
-    def validate_usermessage:
+class MessageManager(models.Manager):
+    def validate_message(self, post_data):
         errors = {}
-        if len(post_data['message']) > 30:
+        if len(post_data['message']) > 300:
             errors['message'] = "Message must be shorter than 300 characters."
         if len(post_data['message']) < 3:
             errors['message'] = "Message must be longer than 3 characters."
+
+
+class TicketManager(models.Manager):
+    def validate_ticket(self, post_data):
+        errors = {}
+        if len(post_data['task']) > 50:
+            errors['task'] = "Task must be shorter than 50 characters."
+        if len(post_data['task']) < 3:
+            errors['task'] = "Task must be longer than 3 characters."
+        if len(post_data['description']) > 500:
+            errors['description'] = "Description must be shorter than 500 characters."
+        if len(post_data['description']) < 3:
+            errors['description'] = "Description must be longer than 3 characters."
+        
+class ChatRoomValidator(models.Manager):
+    def validate_chat(self, post_data):
+        errors = {}
 
 class User(models.Model):
     first_name = models.CharField(max_length=50)
@@ -41,6 +58,8 @@ class User(models.Model):
     
     # messages_created = list of messages this user started
     # past_messages = list of all past messages associated with this user
+    # created_chatrooms = list of chatrooms created by this user
+    # joined_chatrooms = list of chatrooms joined by this user
     # created_tickets = list of tickets created by user
     # current_tickets = list of tickets assigned to user
 
@@ -50,16 +69,14 @@ class User(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
-class UserMessage(models.Model):
+class Message(models.Model):
     message = models.CharField(max_length=500)
 
     message_creator = models.ForeignKey('User', related_name="messages_created", on_delete = models.CASCADE))
     chatroom = models.ForeignKey('ChatRoom', related_name="past_messages", on_delete = models.CASCADE, null=True))
     ticket_chat = models.ForeignKey('Ticket', related_name="past_messages", on_delete = models.CASCADE, null=True))
-    objects = UserMessageManager()
-
-    #created_chatrooms = list of chats user1 has started
-    #joined_chatrooms = list of chats user has joined
+    
+    objects = MessageManager()
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -68,7 +85,9 @@ class ChatRoom(models.Models):
     User1 = models.ForeignKey('User', related_name="created_chatrooms", on_delete = models.CASCADE))
     User2 = models.ForeignKey('User', related_name="joined_chatrooms", on_delete = models.CASCADE))
 
-    #past_messages - list of users message history
+    objects = ChatRoomValidator()
+
+    #past_messages = list of users' message history
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -84,7 +103,7 @@ class Ticket(models.Model):
 
     # past_messages = list of messages associated with this ticket
 
+    objects = TicketManager()
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
